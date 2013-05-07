069a0ec6f27a136d06e90960752b38c96fc5d34b
zsbalogh <baloghzsof@gmail.com>
2013-05-07 09:31:57 +0200

    Initial commit.

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :redmine_special_days do
+  name 'Redmine SpecialDays plugin'
+  author 'LuckyThirteen'
+  description 'This s redmine plugin to handle special days like weekends, holidays etc.'
+  version '0.0.1'
+  #url ''
+end
