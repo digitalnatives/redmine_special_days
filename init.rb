73c4aa741ee0d4e1dc6ae53bf876c9b7e699a542
zsbalogh <baloghzsof@gmail.com>
2013-06-25 14:36:12 +0200

    Plugin settings page for default calendar name.

--- init.rb
+++ init.rb
@@ -8,4 +8,7 @@ Redmine::Plugin.register :redmine_special_days do
   menu :top_menu, 'redmine_special_days', { :controller => 'pocket_calendars', :action => 'index' },
      { :caption => 'redmine_special_days.menu_title'.to_sym,
        :if => Proc.new { User.current.admin } }
+
+  settings :default => {'default_calendar_name' => 'Global'}, :partial => 'settings/redmine_special_days_settings'
+
 end
